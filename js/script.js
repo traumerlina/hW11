f7f970d2d83732f23052855a3cbba318b9f7c88f
traumerlina <linajoldoshbaeva1112@gmail.com>
2022-04-06 19:36:08 +0700

    hi

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let arrayOfNumbers = [100, 55, 15, 300, 20];
+
+let e = 0 + arrayOfNumbers[0];
+document.write(0 + "+" + arrayOfNumbers[0] + "=" + e + "</br>");
+
+l = e + arrayOfNumbers[1];
+document.write( e + "+" + arrayOfNumbers[1] + "=" + l + "</br>");
+
+i = l + arrayOfNumbers[2];
+document.write(l + "+" + arrayOfNumbers[2] + "=" + i + "</br>")
+
+n = i + arrayOfNumbers[3];
+document.write(i + "+" + arrayOfNumbers[3] + "=" + n + "</br>")
+
+a = n + arrayOfNumbers[4];
+document.write(n + "+" + arrayOfNumbers[4] + "=" + n + "</br>")
+
+
+let arrayOfNum = [1100, 132, 688]
